af028f5f959b5dbe316be962912b0c33b8890bb6
Pankaj Sharma <pankaj.sharma@gmail.com>
2023-08-08 07:53:46 +0530

    first commit

---
+++ src/main/java/com/codemind/crs/engine/complaintredressal/ComplaintredressalApplication.java
@@ -0,0 +1,13 @@
+package com.codemind.crs.engine.complaintredressal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintredressalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplaintredressalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codemind/crs/engine/complaintredressal/ComplaintredressalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codemind.crs.engine.complaintredressal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintredressalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
